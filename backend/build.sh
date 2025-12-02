2fda4eec3f3804405a9c3d6e571c23f2b90154a4
Mr-Akio <139813694+Mr-Akio@users.noreply.github.com>
2025-12-02 19:45:14 +0700

    Create superuser mrjo

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# เพิ่มบรรทัดนี้ลงไป (เพื่อสร้าง Admin)
+# พอ Deploy เสร็จแล้ว ให้ลบบรรทัดนี้ออกนะครับ
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('mrjo', 'admin@example.com', '062558') if not User.objects.filter(username='mrjo').exists() else None" | python manage.py shell
