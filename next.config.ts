2e2351c4c7054773f8df4392bb6ff62d4a3cbcba
Mr-Akio <139813694+Mr-Akio@users.noreply.github.com>
2025-12-03 15:35:05 +0700

    Add Docker setup and fix image config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
